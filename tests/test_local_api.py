6348c8c7163bd69f2368b72f70547f5a345bdb4a
DevYeop <lee.sangyeop0715@gmail.com>
2026-01-09 05:28:21 +0900

    chore : 잘못된 환경에서 api 테스트 하는 거 삭제

--- tests/test_local_api.py
+++ tests/test_local_api.py
@@ -12,17 +12,3 @@ import requests
 
 #     # /hello 는 문자열만 내려준다고 가정
 #     assert response.text == "hello"
-
-
-def test_local_get_hello(base_url):
-    """
-    로컬 API(/hello) GET 테스트
-    """
-    url = f"{base_url}/hello"
-
-    response = requests.get(url)
-
-    assert response.status_code == 200
-
-    # /hello 는 문자열만 내려준다고 가정
-    assert response.text == "hello"
